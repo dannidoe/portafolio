f6edf3fc89b70d553ba02a213ad832daf7bdb70c
dannidoe <danielaf.aguilar@gmail.com>
2025-12-10 11:11:52 +0000

    Actualizar título de la pestaña

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,6 @@ export default function Head() {
     <>
       <title>Portafolio · Daniela Aguilar</title>
       <meta name="description" content="Portafolio profesional de Daniela Aguilar, especialista en diseño de redes MEP y BIM" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
     </>
   )
 }
